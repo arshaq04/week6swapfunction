c7fee8838d430db3f1d441b57f616276287d9d2d
Arshaq <arshahm2004@gmail.com>
2024-02-16 16:54:28 +0000

    Swap Function complete

--- SwapFunction.cpp
+++ SwapFunction.cpp
@@ -13,11 +13,17 @@ void print_array(int arrayF[], int size) {
     cout << "]" << endl;
 }
 
+void swap(int arrayC[], int num1, int num2) {
+    int temp = arrayC[num1];
+    arrayC[num1] = arrayC[num2];
+    arrayC[num2] = temp;
+}
+
 int main() {
     int numbers[] = {1, 2, 3};
     print_array(numbers, 3);
     // will produce the output: [1, 2, 3]
-    // swap(numbers, 0, 1);
+    swap(numbers, 0, 1);
     // will produce the output: [2, 1, 3]
     print_array(numbers, 3);
     
